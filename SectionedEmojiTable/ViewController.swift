739e49f6db88cc8d9c1d855d8247cd14a7dc41df
YuryG <yury.gitman@gmail.com>
2016-10-07 14:41:45 -0400

    Added Some Array details

--- SectionedEmojiTable/ViewController.swift
+++ SectionedEmojiTable/ViewController.swift
@@ -20,68 +20,80 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Dispose of any resources that can be recreated.
     }
 
-    //TableView Required Methods
+    //TableView Required Methods  // Called with TableView Loads
     func numberOfSections(in tableView: UITableView) -> Int {
         return 8
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if section == 0{
+            return faceEmoji.count
+            
+        } else if section == 1 {
+            return foodEmoji.count
+        } else if section == 2 {
+            return animalEmoji.count
+        } else if section == 3 {
+            return nerdyEmoji.count
+
+        
+        
+        } else{
+            return 5
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+// return a UITableViewCell
+// work with the indexPath, to get section & row. [for the array element].
         if indexPath.section == 0{
-        let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell")
-        myCell?.textLabel?.text = "Main Text Label💫"
-        myCell?.detailTextLabel?.text = "detail label  : )"
-        return myCell!
+        let myCell = tableView.dequeueReusableCell(withIdentifier:"myCell")
+       myCell?.textLabel?.text = faceEmoji[indexPath.row]
+       myCell?.detailTextLabel?.text =  "Faces"
+            return myCell!
         }
-        
         else if indexPath.section == 1{
             let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell2")
-            myCell?.textLabel?.text = "Main Text BLAH"
-            myCell?.detailTextLabel?.text = "detail label  BLAH BLAH"
+            myCell?.textLabel?.text = foodEmoji[indexPath.row]
+            myCell?.detailTextLabel?.text = "Food"
             return myCell!
 
                 
             }
         else if indexPath.section == 2{
-            let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell")
-            myCell?.textLabel?.text = "Main Text BLAH"
-            myCell?.detailTextLabel?.text = "detail label  BLAH BLAH"
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell3")
+            myCell?.textLabel?.text = animalEmoji[indexPath.row]
+            myCell?.detailTextLabel?.text = "animals"
             return myCell!
             
         }
         else{
-            let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell2")
-            myCell?.textLabel?.text = "Main Text BLAH"
-            myCell?.detailTextLabel?.text = "detail label  BLAH BLAH"
+            let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell4")
+            myCell?.textLabel?.text = nerdyEmoji[indexPath.row]
+            myCell?.detailTextLabel?.text = "nerdy stuff"
             return myCell!
             
         }
         
     }
     
-   
-    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         
-        let myCGFloat = CGFloat(40)
+        let myCGFloat = CGFloat(30)
         return myCGFloat
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
     
         if section == 0{
-        return "Hi, I'm the First Header Title"
+        return "FACES ;)"
         } else if section  == 1{
-            return "Hi, I'm the Second Title"
+            return "FOOD YUM YUMMY"
         
     } else if section  == 2{
-    return "Hi, I'm the 3rd Title"
+    return "CUTE ANIMALS"
     } else if section  == 3{
-    return "Hi, I'm the 4th Title"
+    return "Nerdy Stuff"
     }  else if section  == 4{
         return "Hi, I'm the 5th Title"
     }
@@ -89,6 +101,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             return "Mystery Section"
         }
     }
+    
+    
+    
+    let faceEmoji = ["😀","😁","😂","😃","😆","😇","😈","👿",]   //created array
+    
+    let foodEmoji = ["🍎","🍏","🍐","🍑","🍒","🍓","🍔","🍕","🍖","🍗","🍘","🍙","🍚","🍛","🍜","🍝","🍞","🍟","🍡","🍢","🍣","🍤","🍥","🍦","🍧","🍨","🍩","🍪","🍫"]
+    
+    let animalEmoji = ["🐅","🐆","🐯","🐇","🐰","🐈","🐱","🐴","🐏","🐥","🐦","🐧","🐶","🐺","🐻","🐨","🐼","🐵","🙈","🙉","🙊","🐒","🐢","🐋","🐬","🐙","🐌","🐛","🐜","🐝","🐞"]
+    
+    let nerdyEmoji = ["👓","👒","👞","👟","👕","👔", "🔬","🔭","🔮"]
+
     
     
 }
